b7d914f2bab20b38a70a4327f36e74e5fb7ad39d
OmaymaK <emmablink050@gmail.com>
2022-03-13 05:42:12 -0700

    format check

---
+++ format_check.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * format_check - checks the format according to the input
+ * @s: refers to string format
+ * @ap: argument
+ * Return: length of string or 1 or 0 or -1 if error
+ */
+int format_check (const char s, va_list ap)
+{
+	int i = 0;
+	int len = 0;
+
+	printf_t cha[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_perc},
+		{NULL, NULL},
+	};
+	while (cha[i].ch != NULL)
+	{
+		if (cha[i].ch == s)
+		{
+			len = len + cha[i].func(ap);
+			return (len);
+		}
+		i++;
+	}
+	return (0);
+}
