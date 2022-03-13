31cc0f63ef70be1f53ab485845f11b4de0a5c489
OmaymaK <emmablink050@gmail.com>
2022-03-13 04:28:41 -0700

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <limits.h>
 #include <stdio.h>
-#ifndef printf
-#define printf
+#include <stdarg.h>
+#include <stdlib.h>
 int _printf(const char *format, ...);
+int _putchar(char c);
 #endif
