d8d52def31551dad914296f79e1aa9a8ca855682
OmaymaK <emmablink050@gmail.com>
2022-03-13 04:53:40 -0700

    update

--- main.h
+++ main.h
@@ -1,9 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <limits.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+
+/* functions prototype */
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+
+/* Created functions prototype */
+
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_perc(va_list arg);
 #endif
