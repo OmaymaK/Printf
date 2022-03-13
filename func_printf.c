e212abde864cf4c7d99cff9ebaec33b7597f70fc
FerchichiXIII <anasferchichi22@gmail.com>
2022-03-13 15:27:15 +0100

    update all

--- func_printf.c
+++ func_printf.c
@@ -17,22 +17,22 @@ int print_char(va_list arg)
 int print_string(va_list arg)
 {
 	int i = 0;
-	char *s = va_arg(arg, char*);
+	char *s = va_arg(arg, char *);
 	while (s[i] != '\0')
 	{
 		_putchar(*s);
-		i++
+		i++;
 	}
 	return (i);
 }
 /**
- * print_perc - prints percentage 
- * @arg: attribute unused 
+ * print_perc - prints percentage
+ * @arg: attribute unused
  * Return: 1
  */
 int print_perc(va_list arg)
 {
-	void(arg)
-	_putchar('%');
+	void(arg);
+		_putchar('%');
 	return (1);
 }
