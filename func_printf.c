deeeb3e1fc4699a5ce298c5718a1061344b52467
OmaymaK <emmablink050@gmail.com>
2022-03-13 04:24:45 -0700

    printf functions

---
+++ func_printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_char - prints character
+ * @arg: the character to be printed
+ * Return: 1
+ */
+int print_char(va_list arg)
+{
+	_putchar(va_arg(arg, int));
+	return (1);
+}
+/**
+ * print_string - prints the string
+ * @arg: the string to be printed
+ * Return: length of the string
+ */
+int print_string(va_list arg)
+{
+	int i = 0;
+	char *s = va_arg(arg, char*);
+	while (s[i] != '\0')
+	{
+		_putchar(*s);
+		i++
+	}
+	return (i);
+}
+/**
+ * print_perc - prints percentage 
+ * @arg: attribute unused 
+ * Return: 1
+ */
+int print_perc(va_list arg)
+{
+	void(arg)
+	_putchar('%');
+	return (1);
+}
