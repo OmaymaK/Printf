803498ff74612218280398fa1b05001ebc278c54
OmaymaK <97183204+OmaymaK@users.noreply.github.com>
2022-03-14 12:16:16 +0100

    Update func_printf.c

--- func_printf.c
+++ func_printf.c
@@ -13,19 +13,15 @@ int print_char(va_list arg)
  * print_string - prints the string
  * @arg: the string to be printed
  * Return: length of the string
- */
+ */     
 int print_string(va_list arg)
 {
 	int i = 0;
-<<<<<<< HEAD
-	char *s = va_arg(arg, char *);
-=======
 	char *s = va_arg(arg, char*);
-
->>>>>>> dd2610b4e5f7337488281dcff3ae0c50ec77d9ed
+	
 	while (s[i] != '\0')
 	{
-		_putchar(*s);
+		_putchar(s[i]);
 		i++;
 	}
 	return (i);
@@ -37,11 +33,6 @@ int print_string(va_list arg)
  */
 int print_perc(va_list arg __attribute__((unused)))
 {
-<<<<<<< HEAD
-	void(arg);
-		_putchar('%');
-=======
 	_putchar('%');
->>>>>>> dd2610b4e5f7337488281dcff3ae0c50ec77d9ed
 	return (1);
 }
